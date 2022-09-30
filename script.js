4adcf231473352e2fcd6a72a52bb4458cd7639c2
AF Tamim <itamim345@gmail.com>
2022-10-01 02:21:48 +0530

    Basic JS

---
+++ script.js
@@ -0,0 +1,4 @@
+let plusBtn = document.getElementById('plus-btn');
+let minusBtn = document.getElementById('minus-btn');
+let qty = document.getElementById('qty');
+let total = document.getElementById('total');
