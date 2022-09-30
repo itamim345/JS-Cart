6554e0bb929c8e50c4df90da6d19baabbd857ce6
AF Tamim <itamim345@gmail.com>
2022-10-01 02:33:12 +0530

    Plus Buttons JS Added

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let plusBtn = document.getElementById('plus-btn');
 let minusBtn = document.getElementById('minus-btn');
 let qty = document.getElementById('qty');
 let total = document.getElementById('total');
+
+// Event Listener to Add product with Calculation
+plusBtn.addEventListener('click', function(){
+    let qtyInt = parseInt(qty.innerText);
+    qtyInt ++;
+    qty.innerText = qtyInt;
+    total.innerText = 2500*qtyInt;
+})
